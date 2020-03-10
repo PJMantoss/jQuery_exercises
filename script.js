b4f672b40ef9a4f63a36646717472626591f1e59
pjmantoss <joelptoss@gmail.com>
2020-03-10 16:40:00 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Q1. When the DOM is ready, console.log the message "Let's get ready to party with jQuery!"
 $(document).ready(function(){
     console.log("Let's get ready to party with jQuery!")
+//Q2.Give all images inside of an article tag the class of image-center (this class is defined inside of the style tag in the head). 
 })
