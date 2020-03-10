36d74e072163f0888198b25b84477bfed32338dd
pjmantoss <joelptoss@gmail.com>
2020-03-10 18:20:43 +0100

    update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     console.log("Let's get ready to party with jQuery!");
 
 //Q2.Give all images inside of an article tag the class of image-center (this class is defined inside of the style tag in the head). 
-$("article").addClass("image-center");
+$("article img").addClass("image-center");
 
 //Q3.Remove the last paragraph in the article.
 $("p").eq(5).remove();
