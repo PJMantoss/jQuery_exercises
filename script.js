4cff04d9d20b715c822f6ce3a2b2e669fe5ebd33
pjmantoss <joelptoss@gmail.com>
2020-03-10 17:20:40 +0100

    update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 //Q2.Give all images inside of an article tag the class of image-center (this class is defined inside of the style tag in the head). 
 $("article").addClass("image-center");
 //Q3.Remove the last paragraph in the article.
-$("article")
-//Q4.Remove the last paragraph in the article.
 $("p").eq(5).remove();
+//Q4.Set the font size of h1 with an id of title to be a random pixel size from 0 to 100.
+$()
 })
